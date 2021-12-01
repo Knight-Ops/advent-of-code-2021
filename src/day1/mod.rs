0c7125420b436948120923d5112bae2cae2a85de
Carl Hurd <carl@basilisklabs.com>
2021-12-01 10:04:13 -0500

    day 1 done

---
+++ src/day1/mod.rs
@@ -0,0 +1,71 @@
+pub fn input_generator(input: &str) -> Vec<u32> {
+    input
+        .split("\n")
+        .into_iter()
+        .map(|num| num.parse::<u32>().expect("Error parsing &str into u32"))
+        .collect()
+}
+
+pub fn part1(input: &[u32]) -> usize {
+    input
+        .into_iter()
+        .enumerate()
+        .skip(1)
+        .filter(|(idx, val)| val > &&input[idx - 1])
+        .count()
+}
+
+pub fn part2(input: &[u32]) -> usize {
+    let sliding_window: Vec<u32> = input
+        .iter()
+        .enumerate()
+        .take_while(|(idx, _)| *idx <= input.len() - 3)
+        .map(|(idx, x)| x + input[idx + 1] + input[idx + 2])
+        .collect();
+
+    sliding_window
+        .iter()
+        .enumerate()
+        .take_while(|(idx, _)| *idx < sliding_window.len() - 1)
+        .filter(|(idx, x)| **x < sliding_window[idx + 1])
+        .count()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_test() {
+        let i = "199
+200
+208
+210
+200
+207
+240
+269
+260
+263";
+
+        let input = input_generator(i);
+        assert_eq!(part1(&input), 7);
+    }
+
+    #[test]
+    fn part2_test() {
+        let i = "199
+200
+208
+210
+200
+207
+240
+269
+260
+263";
+
+        let input = input_generator(i);
+        assert_eq!(part2(&input), 5);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use advent_of_code_2021 as aoc;
+
+fn main() {
+    day1();
+}
+
+fn day1() {
+    let raw_input = aoc::read_input_file("input/2021/day1.txt");
+
+    let formatted_input = aoc::day1::input_generator(&raw_input);
+
+    let part1 = aoc::day1::part1(&formatted_input);
+    println!("Solution for Day 1 Part 1 : {}", part1);
+
+    let part2 = aoc::day1::part2(&formatted_input);
+    println!("Solution for Day 1 Part 2: {}", part2);
+}
